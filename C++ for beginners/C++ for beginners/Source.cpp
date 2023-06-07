8d53c786ccc567ad9d01f2177f9ab22dfeac9175
Anto Martínez <antomartinezdev@gmail.com>
2023-06-07 19:37:52 +0200

    Primeras variables creadas y mostradas por pantalla

--- C++ for beginners/C++ for beginners/Source.cpp
+++ C++ for beginners/C++ for beginners/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "You died!/n";
+	char myCharacter;
+	myCharacter = 'y';
+
+	int myInt;
+	myInt = 5;
+
+	cout << myCharacter << endl;
+	cout << myInt << endl;
+
+	myCharacter = 'n';
+	myInt = 7;
+
+	cout << myCharacter << endl;
+	cout << myInt << endl;
 
 	system("pause");
 }
